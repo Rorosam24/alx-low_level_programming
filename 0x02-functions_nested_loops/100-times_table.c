0bada448cf079ce4630dd06844b658e18e7c352f
Rorosam24 <Robawalaaeldin@gmail.com>
2022-09-14 17:55:40 -0700

    timestable

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_times_table - print the times table
+ *
+ * Return: Always success
+ */
+print_times_table(int n) 
+{
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
+}
